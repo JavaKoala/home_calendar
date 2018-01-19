83bd56adb84e4d23db358ded7ac8c97b1b60ff62
Ben Meyer <bmeyer@medsagetechnologies.com>
2018-01-18 19:54:28 -0500

    Added create event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,14 @@ class EventsController < ApplicationController
   def new
     @event = Event.new
   end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:title, :start, :end, :color)
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,6 @@
 class Event < ApplicationRecord
+
+  def all_day_event?
+    self.start == self.start.midnight && self.end == self.end.midnight ? true : false
+  end
 end
