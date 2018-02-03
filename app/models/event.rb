81f0641ee22d0f8b36bed0a740579b9e88db911f
Ben Meyer <bmeyer@medsagetechnologies.com>
2018-02-03 10:50:10 -0500

    Added validation so the end of the event cannot be before the start

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,11 @@
 class Event < ApplicationRecord
+  validate :end_before_start
 
   def all_day_event?
     self.start == self.start.midnight && self.end == self.end.midnight ? true : false
+  end
+
+  def end_before_start
+    errors.add(:start, "End can't be before start") if self.end < self.start
   end
 end
