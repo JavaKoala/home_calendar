05d4880df5f8c7ce228f6c92e0fe8e5309ef9877
Ben Meyer <bmeyer@medsagetechnologies.com>
2018-05-27 09:41:05 -0400

    Changed events character set to utf8mb4 Changed events collation to utf8mb4_bin Should now support emojis

---
+++ db/migrate/20180527131421_utf8mb4.rb
@@ -0,0 +1,5 @@
+class Utf8mb4 < ActiveRecord::Migration[5.1]
+  def change
+    execute "ALTER TABLE events CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;"
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180117004042) do
+ActiveRecord::Schema.define(version: 20180527131421) do
 
-  create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin" do |t|
     t.string "title"
     t.datetime "start"
     t.datetime "end"
