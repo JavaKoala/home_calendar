7cfc37eb8cbd2852ae8558ad5cc530361f70375d
Ben Meyer <bmeyer@medsagetechnologies.com>
2019-09-22 11:04:56 -0400

    Update to config/deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+lock "~> 3.11.1"
 
 set :application, "home_calendar"
 set :repo_url, "https://github.com/JavaKoala/home_calendar.git"
