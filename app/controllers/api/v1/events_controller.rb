15798aac4be77a8fd2f6a04c0e83079b8228bdac
JavaKoala <javakoala1@gmail.com>
2024-11-25 18:58:10 -0500

    Fill in show method

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -2,8 +2,13 @@ module Api
   module V1
     class EventsController < ApplicationController
       def show
-        @event = Event.find(params[:id])
-        render json: @event
+        @event = Event.find_by(id: params[:id])
+
+        if @event.present?
+          render json: @event
+        else
+          render status: :not_found, json: {}
+        end
       end
     end
   end
--- test/controllers/api/v1/events_controller_test.rb
+++ test/controllers/api/v1/events_controller_test.rb
@@ -7,6 +7,21 @@ module Api
         get api_v1_event_url(Event.first.id)
         assert_response :success
       end
+
+      test 'should return event' do
+        get api_v1_event_url(Event.first.id)
+        assert_equal Event.first.id, JSON.parse(@response.body)['id']
+        assert_equal Event.first.title, JSON.parse(@response.body)['title']
+        assert_equal Event.first.start, JSON.parse(@response.body)['start']
+        assert_equal Event.first.end, JSON.parse(@response.body)['end']
+        assert_equal Event.first.color, JSON.parse(@response.body)['color']
+      end
+
+      test 'should empty json for nonexistant event' do
+        get api_v1_event_url(Event.last.id + 1)
+        assert_response :not_found
+        assert_equal '{}', @response.body
+      end
     end
   end
 end
