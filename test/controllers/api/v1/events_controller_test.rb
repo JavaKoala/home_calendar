f2f7ca902749e7c1afcf8520a3ed5b7ec8a82e47
JavaKoala <javakoala1@gmail.com>
2024-11-25 18:59:54 -0500

    Refactor test

--- test/controllers/api/v1/events_controller_test.rb
+++ test/controllers/api/v1/events_controller_test.rb
@@ -10,17 +10,17 @@ module Api
 
       test 'should return event' do
         get api_v1_event_url(Event.first.id)
-        assert_equal Event.first.id, JSON.parse(@response.body)['id']
-        assert_equal Event.first.title, JSON.parse(@response.body)['title']
-        assert_equal Event.first.start, JSON.parse(@response.body)['start']
-        assert_equal Event.first.end, JSON.parse(@response.body)['end']
-        assert_equal Event.first.color, JSON.parse(@response.body)['color']
+        assert_equal Event.first.id, response.parsed_body['id']
+        assert_equal Event.first.title, response.parsed_body['title']
+        assert_equal Event.first.start, response.parsed_body['start']
+        assert_equal Event.first.end, response.parsed_body['end']
+        assert_equal Event.first.color, response.parsed_body['color']
       end
 
       test 'should empty json for nonexistant event' do
         get api_v1_event_url(Event.last.id + 1)
         assert_response :not_found
-        assert_equal '{}', @response.body
+        assert_equal '{}', response.body
       end
     end
   end
