66a3820630529207f51ddfd60c0f49cd5e2a9c38
JavaKoala <javakoala1@gmail.com>
2026-04-25 12:37:09 -0400

    Remove capybara from test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,16 +11,7 @@ require 'capybara/minitest'
 
 module ActiveSupport
   class TestCase
-    include Capybara::DSL
-    include Capybara::Minitest::Assertions
-
-    Capybara.default_driver = :selenium_chrome_headless
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    def teardown
-      Capybara.reset_sessions!
-      Capybara.use_default_driver
-    end
   end
 end
