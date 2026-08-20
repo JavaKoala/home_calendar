a04f1ae719d928e6594edbd9585553296021a9c9
JavaKoala <javakoala1@gmail.com>
2026-08-20 19:23:06 -0400

    Refactor service

--- app/services/import_i_cal_service.rb
+++ app/services/import_i_cal_service.rb
@@ -2,12 +2,21 @@ class ImportICalService
   ImportResult = Data.define(:success?, :error_message)
 
   def initialize(ics_file)
-    @ics_file = ics_file
+    @parser = Icalendar::Parser.new(ics_file, true)
   end
 
   def import
-    parser = Icalendar::Parser.new(@ics_file, true)
-    calendars = parser.parse
+    calendars = @parser.parse
+    import_calendars(calendars)
+
+    ImportResult.new(success?: true, error_message: nil)
+  rescue Icalendar::Parser::ParseError, ActiveRecord::RecordInvalid => e
+    ImportResult.new(success?: false, error_message: e.message)
+  end
+
+  private
+
+  def import_calendars(calendars)
     calendars.each do |calendar|
       events = calendar.events
       events.each do |event|
@@ -18,9 +27,5 @@ class ImportICalService
         )
       end
     end
-
-    ImportResult.new(success?: true, error_message: nil)
-  rescue Icalendar::Parser::ParseError, ActiveRecord::RecordInvalid => e
-    ImportResult.new(success?: false, error_message: e.message)
   end
 end
