33805e4ae3410f45a8e21a91a09a3f4a78488cab
JavaKoala <javakoala1@gmail.com>
2026-09-02 23:42:51 -0400

    Remove db name

--- db/migrate/20260903032056_backfill_ics_uid.rb
+++ db/migrate/20260903032056_backfill_ics_uid.rb
@@ -1,7 +1,7 @@
 class BackfillIcsUid < ActiveRecord::Migration[8.1]
   def up
     execute <<~SQL.squish
-      UPDATE home_calendar_development.events
+      UPDATE events
       SET ics_uid = UUID()
       WHERE ics_uid IS NULL;
     SQL
